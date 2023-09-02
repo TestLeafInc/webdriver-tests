37424320dfd45069aa80bb3f6a134da478b31e48
Abhinay Lunawat <abhinaylunawat5993@gmail.com>
2023-09-02 11:01:31 +0530

    adding PD page validation test scripts with 10 TC's AL

---
+++ src/main/java/com/leaftaps/testcases/TC03_Validate_PD_Page_AL.java
@@ -0,0 +1,28 @@
+package com.leaftaps.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.framework.testng.api.base.ProjectHooks;
+import com.leaftaps.pages.LoginPage;
+
+public class TC001_JustLogin extends ProjectHooks{
+	@BeforeTest
+	public void setValues() {
+		testcaseName = "Login";
+		testDescription ="Login to Leaftaps";
+		authors="Babu";
+		category ="Smoke";
+		excelFileName="Login";
+	}
+	
+	@Test(dataProvider = "fetchData")
+	public void runLogin(String username, String password) {
+		new LoginPage()
+		.enterUsername(username)
+		.enterPassword(password)
+		.clickLogin();
+
+	}
+
+}
