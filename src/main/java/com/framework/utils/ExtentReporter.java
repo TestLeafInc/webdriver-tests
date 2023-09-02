47e05ced6ef4be421067df11b827544711c1d4e0
Manickam B <bmanickamraj@gmail.com>
2023-09-02 12:37:18 +0530

    changes to extent report by Manickam

--- src/main/java/com/framework/utils/ExtentReporter.java
+++ src/main/java/com/framework/utils/ExtentReporter.java
@@ -46,10 +46,10 @@ public abstract class ExtentReporter extends DriverInstance {
 		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("./" + folderName + "/" + fileName);
 		htmlReporter.config().setTestViewChartLocation(ChartLocation.BOTTOM);
 		htmlReporter.config().setChartVisibilityOnOpen(!true);
-		htmlReporter.config().setTheme(Theme.STANDARD);
-		htmlReporter.config().setDocumentTitle("Leaftaps");
+		htmlReporter.config().setTheme(Theme.DARK);
+		htmlReporter.config().setDocumentTitle("LeaftapsTEST");
 		htmlReporter.config().setEncoding("utf-8");
-		htmlReporter.config().setReportName("Leaftaps");
+		htmlReporter.config().setReportName("LeaftapsTEST");
 		htmlReporter.setAppendExisting(true);
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
