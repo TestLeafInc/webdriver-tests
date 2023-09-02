9ffbe8c4f7ca2e424f56c66af22b290e2caddd5e
mukesh012 <mukesh012@live.com>
2023-09-02 13:18:57 +0530

    test changes

--- src/main/java/com/leaftaps/testcases/test03.java
+++ src/main/java/com/leaftaps/testcases/test03.java
@@ -4,12 +4,9 @@ public class test03 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-
-System.out.println(test2);
-=======
+System.out.println("test2");
 System.out.println("this is test");
->>>>>>> f4e61759032e5373e52b7b65cb833b4f06802fa8
+
 	}
 
 }
