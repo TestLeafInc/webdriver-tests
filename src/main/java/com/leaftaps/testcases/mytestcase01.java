11e1e65402c76dd2fce5ffe40ff1bd3a11d1c882
SARANYASUNDARARAJAN09 <saranyarajan6165@gmail.com>
2023-09-02 11:10:09 +0530

    my test

---
+++ src/main/java/com/leaftaps/testcases/mytestcase01.java
@@ -0,0 +1,11 @@
+package com.leaftaps.testcases;
+
+public class mytestcase01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my new class");
+
+	}
+
+}
