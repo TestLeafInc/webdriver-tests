36cc8c9dc78ae39950d6ff78804b1383377d4723
subash <subash.devaraj07@gmail.com>
2023-09-02 11:03:01 +0530

    pushing file TC003

--- src/main/java/com/leaftaps/testcases/TC003_JustLogin.java
+++ src/main/java/com/leaftaps/testcases/TC003_JustLogin.java
@@ -6,12 +6,12 @@ import org.testng.annotations.Test;
 import com.framework.testng.api.base.ProjectHooks;
 import com.leaftaps.pages.LoginPage;
 
-public class TC002_JustLogin extends ProjectHooks{
+public class TC003_JustLogin extends ProjectHooks{
 	@BeforeTest
 	public void setValues() {
 		testcaseName = "Login";
 		testDescription ="Login to Leaftaps";
-		authors="Lakshmi";
+		authors="Subash";
 		category ="Smoke";
 		excelFileName="Login";
 	}
