51525efbfc2492e70ff8baf26598643173549644
Keerthi <mkeerthana.114@gmail.com>
2023-12-17 11:58:56 +0530

    git branch added testcase 4

---
+++ src/main/java/com/leaftaps/testcases/TC001_JustLogin4.java
@@ -0,0 +1,29 @@
+
+package com.leaftaps.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.framework.testng.api.base.ProjectHooks;
+import com.leaftaps.pages.LoginPage;
+
+public class TC001_JustLogin4 extends ProjectHooks{
+	@BeforeTest
+	public void setValues() {
+		testcaseName = "Login";
+		testDescription ="Login to Leaftaps";
+		authors="Keerthi123";
+		category ="Smoke";
+		excelFileName="Login";
+	}
+	
+	@Test(dataProvider = "fetchData")
+	public void runLogin(String username, String password) {
+		new LoginPage()
+		.enterUsername(username)
+		.enterPassword(password)
+		.clickLogin();
+
+	}
+
+}
